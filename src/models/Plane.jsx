b0725696b5c5944c934be2e65937c6770928724a
Mateus Souza <msouza954@gmail.com>
2023-11-16 16:39:10 -0300

    feat:move island and plane

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import { useRef, useEffect } from "react";
 import planeScene from "../assets/3d/plane.glb";
-import { useGLTF } from "@react-three/drei";
-export const Plane = () => {
+import { useAnimations, useGLTF } from "@react-three/drei";
+export const Plane = ({ isRotating, ...props }) => {
+  const ref = useRef();
   const { scene, animations } = useGLTF(planeScene);
+  const { actions } = useAnimations(animations, ref);
+
+  useEffect(() => {
+    if (isRotating) {
+      actions["Take 001"].play();
+    } else {
+      actions["Take 001"].stop();
+    }
+  }, [isRotating, actions]);
+
   return (
-    <mesh>
+    <mesh {...props} ref={ref}>
       <primitive object={scene} />
     </mesh>
   );
