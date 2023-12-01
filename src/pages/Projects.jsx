a9b5f77dc32f94d56b37cc91e0bdba2e4d1b7357
Mateus Souza <msouza954@gmail.com>
2023-12-01 12:32:19 -0300

    finished

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom";
+import { projects } from "../constants";
+import { arrow } from "../assets/icons";
+import { CTA } from "../components/CTA";
 export const Projects = () => {
   return (
     <section className="max-container">
@@ -18,6 +22,48 @@ export const Projects = () => {
           para melhorias adicionais. Sua colaboração é altamente valorizada!
         </p>
       </div>
+
+      <div className="flex flex-wrap my-20 gap-16">
+        {projects.map((project) => (
+          <div className="lg:w-[400px] w-full" key={project.name}>
+            <div className="block-container w-12 h-12">
+              <div className={`btn-back rounded-xl ${project.theme}`} />
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <img
+                  src={project.iconUrl}
+                  alt="Project Url"
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+
+            <div className="mt-5 flex flex-col">
+              <h4 className="subhead-text">{project.name}</h4>
+              <p className="mt-2 text-slate-500">{project.description}</p>
+              <div>
+                <Link
+                  to={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="font-semibold text-blue-600"
+                >
+                  Live Link
+                </Link>
+                <img
+                  src={arrow}
+                  alt="arrow"
+                  className=" w-4 h-4 object-contain"
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <hr className="border-slate-200" />
+
+      <CTA />
     </section>
   );
 };
+1;
